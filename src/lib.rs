a0b089d4afb0b8274a0d05049db9d81ef67407bf
Alistair Francis <alistair.francis@wdc.com>
2020-10-27 15:31:20 -0700

    Initial commit
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+//! libMCTP is a MCTP (Management Component Transport Protocol) implementation
+//! for Rust.
+//!
+//! libMCTP aims to implement the MCTP protocol as described in the DMTF DSP2016
+//! specification, which can be found here:
+//! https://www.dmtf.org/sites/default/files/standards/documents/DSP2016.pdf
+//!
+//! MCTP allows multiple transport layers, the protocols supported by this library
+//! include:
+//!  * SMBus/I2C version 1.2.0. See DMTF DSP0237 (https://www.dmtf.org/sites/default/files/standards/documents/DSP0237_1.2.0.pdf)
+//!
+
+#![no_std]
+#![deny(missing_docs)]
+
